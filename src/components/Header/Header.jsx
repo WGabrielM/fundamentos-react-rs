9e636d9f88dcd7675e56a32d887be7ef2660ac4b
Wellington Gabriel Monteiro Batista <wellingtongmb@gmail.com>
2023-10-18 12:19:55 -0300

    add header

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import styles from "./style/Header.module.css"
+
+export function Header() {
+  return (
+    <header>
+        <strong className={styles.header}>Feed Ignite</strong>
+    </header>
+  )
+}
