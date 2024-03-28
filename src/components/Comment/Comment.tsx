de92ccb3a75733fe06d0490e1f3737d03b7b4edc
Wellington Gabriel Monteiro Batista <wellingtongmb@gmail.com>
2024-03-28 11:14:02 -0300

    change project for TS

--- src/components/Comment/Comment.tsx
+++ src/components/Comment/Comment.tsx
@@ -3,7 +3,12 @@ import { ThumbsUp, Trash } from "phosphor-react";
 import styles from "./Comment.module.css";
 import { useState } from "react";
 
-export function Comment({ content, onDeleteComment }) {
+interface CommentProps {
+  content: string;
+  onDeleteComment: (comment: string) => void;
+}
+
+export function Comment({ content, onDeleteComment }: CommentProps) {
   const [likeCount, setLikeCount] = useState(0);
 
   function handleLikeComment() {
@@ -18,7 +23,7 @@ export function Comment({ content, onDeleteComment }) {
 
   return (
     <div className={styles.comment}>
-      <Avatar hasBorder={false} src="https://github.com/WGabrielM.png" />
+      <Avatar hasBorder={false} src="https://github.com/WGabrielM.png" alt="" />
 
       <div className={styles.commentBox}>
         <div className={styles.commentContent}>
--- src/components/Post/Post.tsx
+++ src/components/Post/Post.tsx
@@ -1,13 +1,31 @@
-/* eslint-disable react/jsx-key */
-import { format, formatDistanceToNow } from "date-fns";
 import ptBR from "date-fns/locale/pt-BR";
+import { format, formatDistanceToNow } from "date-fns";
 
 import { Avatar } from "../Avatar/Avatar";
 import { Comment } from "../Comment/Comment";
-import styles from "./Post.module.css";
-import { useState } from "react";
+import { ChangeEvent, FormEvent, InvalidEvent, useState } from "react";
 
-export function Post({ author, publishedAt, content }) {
+import styles from "./Post.module.css";
+
+interface Author {
+  name: string;
+  role: string;
+  avatarUrl: string;
+}
+
+interface Content {
+  type: "paragraph" | "link";
+  content: string;
+}
+
+interface PostProps {
+  author: Author;
+  publishedAt: Date;
+  content: Content[];
+}
+
+
+export function Post({ author, publishedAt, content }: PostProps) {
   const [comments, setComments] = useState(["Post muito bana hein!"]);
 
   const [newCommentText, setNewCommentText] = useState("");
@@ -25,25 +43,25 @@ export function Post({ author, publishedAt, content }) {
     addSuffix: true,
   });
 
-  function handleCreateNewComment() {
+  function handleCreateNewComment(event: FormEvent) {
     event.preventDefault();
     setComments([...comments, newCommentText]);
     setNewCommentText("");
   }
 
-  function handleNewCommentChange() {
-    event.target.setCustomValidity('');
+  function handleNewCommentChange(event: ChangeEvent<HTMLTextAreaElement>) {
+    event.target.setCustomValidity("");
     setNewCommentText(event.target.value);
   }
 
-  function handleNewCommentInvalid() {
-    event.target.setCustomValidity('Esse campo é obrigatório');
+  function handleNewCommentInvalid(event: InvalidEvent<HTMLTextAreaElement>) {
+    event.target.setCustomValidity("Esse campo é obrigatório");
   }
 
-  function deleteComment(commentToDelete) {
-    const commentsWithoutDeletedOne = comments.filter(comment => {
+  function deleteComment(commentToDelete: string) {
+    const commentsWithoutDeletedOne = comments.filter((comment) => {
       return comment !== commentToDelete;
-    })
+    });
 
     setComments(commentsWithoutDeletedOne);
   }
@@ -96,7 +114,9 @@ export function Post({ author, publishedAt, content }) {
         />
 
         <footer>
-          <button type="submit" disabled={isNewCommentEmpty}>Publicar</button>
+          <button type="submit" disabled={isNewCommentEmpty}>
+            Publicar
+          </button>
         </footer>
       </form>
 
